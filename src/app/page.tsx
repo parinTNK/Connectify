641aa8f025135ff3f67570549f01792ab10ca196
parinTNK <parin.tnk@gmail.com>
2025-04-06 21:34:04 +0700

    feat: setup responsive navigation bar with dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
-import { SignInButton, SignUpButton, UserButton, SignedOut, SignedIn } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button"
-import ModeToggle from "@/components/ModeToggle";
-
-
 
 export default function Home() {
   return (
     <>
-    <div className="flex items-center justify-between p-4">
-      <SignedOut>
-        <SignInButton mode="modal" >
-          <Button variant="default" size="lg">
-            Sign In
-          </Button>
-        </SignInButton>
-        <SignUpButton mode="modal" />
-      </SignedOut>
-      <ModeToggle />
-
-      {/* after singin will see */}
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+     
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import { SignInButton, SignUpButton, UserButton, SignedOut, SignedIn } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button"
+import ModeToggle from "@/components/ModeToggle";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { AlignJustify } from "lucide-react";
+
+import Link from "next/link";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
+
+function Navbar() {
+  return (
+    
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+        <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
+              Connectify
+            </Link>
+          </div>
+
+          <DesktopNavbar />
+          <MobileNavbar />
+        </div>
+      </div>
+    </nav>
+    
+  )
+}
+
+export default Navbar
